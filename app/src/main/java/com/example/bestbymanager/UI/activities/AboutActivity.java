685130e1f7b057ca398983938a930550b8e0e11e
Josh Werlein <jwerlein2@student.cvtc.edu>
2025-07-18 12:15:27 -0500

    Added about page

--- app/src/main/java/com/example/bestbymanager/UI/activities/AboutActivity.java
+++ app/src/main/java/com/example/bestbymanager/UI/activities/AboutActivity.java
@@ -2,9 +2,13 @@ package com.example.bestbymanager.UI.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import com.example.bestbymanager.R;
 import com.example.bestbymanager.databinding.ActivityAboutBinding;
 
@@ -16,6 +20,22 @@ public class AboutActivity extends AppCompatActivity {
         setTitle(R.string.about);
         ActivityAboutBinding binding = ActivityAboutBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        TextView email = findViewById(R.id.dev_email);
+        TextView linkedin = findViewById(R.id.dev_linkedin);
+        TextView license = findViewById(R.id.license_footer);
+
+        email.setText(Html.fromHtml(getString(R.string.dev_email), Html.FROM_HTML_MODE_LEGACY));
+        email.setMovementMethod(LinkMovementMethod.getInstance());
+
+        linkedin.setText(Html.fromHtml(getString(R.string.dev_linkedin), Html.FROM_HTML_MODE_LEGACY));
+        linkedin.setMovementMethod(LinkMovementMethod.getInstance());
+
+        license.setText(Html.fromHtml(getString(R.string.license_footer), Html.FROM_HTML_MODE_LEGACY));
+        license.setMovementMethod(LinkMovementMethod.getInstance());
+
+        email.setLinkTextColor(ContextCompat.getColor(this, R.color.dark_green));
+        linkedin.setLinkTextColor(ContextCompat.getColor(this, R.color.dark_green));
+        license.setLinkTextColor(ContextCompat.getColor(this, R.color.dark_green));
     }
 
     @Override
